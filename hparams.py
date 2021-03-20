3a4e10fbf704f31156615522f63dc64a19ffe7d1
spectraldoy <littleguyisfum@gmail.com>
2021-03-20 14:08:49 +0530

    updated license

--- hparams.py
+++ hparams.py
@@ -1,14 +1,14 @@
 """
 Copyright 2020 Aditya Gomatam.
 
-This file is part of Music-Transformer (https://github.com/spectraldoy/Music-Transformer), my project to build and
-train a Music Transformer. Music-Transformer is open-source software licensed under the terms of the GNU General
-Public License v3.0. Music-Transformer is free software: you can redistribute it and/or modify it under the terms of
+This file is part of music-transformer (https://github.com/spectraldoy/music-transformer), my project to build and
+train a Music Transformer. music-transformer is open-source software licensed under the terms of the GNU General
+Public License v3.0. music-transformer is free software: you can redistribute it and/or modify it under the terms of
 the GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
-or (at your option) any later version. Music-Transformer is distributed in the hope that it will be useful,
+or (at your option) any later version. music-transformer is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details. A copy of this license can be found within the GitHub repository
-for Music-Transformer, or at https://www.gnu.org/licenses/gpl-3.0.html.
+for music-transformer, or at https://www.gnu.org/licenses/gpl-3.0.html.
 """
 
 from torch import cuda, device as d
--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
+"""
+Copyright 2020 Aditya Gomatam.
+
+This file is part of music-transformer (https://github.com/spectraldoy/music-transformer), my project to build and
+train a Music Transformer. music-transformer is open-source software licensed under the terms of the GNU General
+Public License v3.0. music-transformer is free software: you can redistribute it and/or modify it under the terms of
+the GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version. music-transformer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details. A copy of this license can be found within the GitHub repository
+for music-transformer, or at https://www.gnu.org/licenses/gpl-3.0.html.
+"""
+
 from setuptools import setup, find_packages
 
 setup(name="music-transformer", version='1.0.0', packages=find_packages())
